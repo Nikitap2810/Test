b5498d356e8015e840d714d720ba7937c953576e
Nikitap2810 <118251297+Nikitap2810@users.noreply.github.com>
2022-12-01 15:27:01 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,8 @@
 import React from "react";
 
-import NewProject from "./src/screen/homepg";
-import Home from "./src/screen/reduxexample/home";
-import HomePage from "./src/screen/technical/Loginpage";
-//import Home from "./src/screen/r";
-//import Firstpage from "./src/screen/technical/Loginpage";
+
 import Test from "./src/screen/Test";
-import FirstPage from "./src/screen/reduxexample/firstpg";
-import TodoList from "./src/screen/TodoList";
+
 
 export default class App extends React.Component{
     constructor(){
@@ -27,12 +22,6 @@ export default class App extends React.Component{
 
         <Test/>
 
-      // <Provider store={store}>
-      // <FirstPage/>
-      //   </Provider>
-   
-          
-      
       )
       
     }
